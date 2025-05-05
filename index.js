8f32013e9e1e747dabb76d4ecb33df60ee787f76
mlaundry123 <mackenzie.laundry123@gmail.com>
2025-05-05 17:38:41 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+let color = "black";
+let click = false;
+
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+
+    document.querySelector("body").addEventListener("click"), function(e){
+        if(e.target.tagName != "BUTTON")
+            click = !click;
+        let draw = document.querySelector("#draw");
+        if(click){
+            draw.innerHTML = "Now You Can Draw";
+        }
+        else{
+            draw.innerHTML = "You're Now Allowed To Draw"
+        }
+    }
+})
+    
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
+
+function createBoard(size){
+    let board = document.querySelector(".board");
+
+    board.computedStyleMap.gridTemplateColumns = `repeat($(size), 1fr)`;
+    board.computedStyleMap.gridTemplateRows = `repeat($(size), 1fr)`;
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+       div.addEventListener("mouseover", colorDiv);
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize(){
+    let input = prompt("what will be the size of he board?");
+    let message = document.querySelector("message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100";
+    }
+    else{
+        message.innerHTML = "Now you play!";
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(click){}
+        if(color == "random"){
+            this.style.backgroundColor = `hsl($(Math.random() * 360), 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = 'black'
+        }
+}
+
+function setColor(colorChoice){
+    let color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectionAll("divs")
+    divs.forEah((div) => div.style.backgroundColor = "white")
+}
